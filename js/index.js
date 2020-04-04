68a43d25733e3bd73facc9a6d084a3ad82baa947
mvanegas10 <m.vanegas10@uniandes.edu.co>
2020-04-04 15:56:50 +0200

    Add third waypoint

--- js/index.js
+++ js/index.js
@@ -1,44 +1,31 @@
 const initialize = async _ => {
-    console.log('initializing')
-
     d3.select('#date').text(`${lastUpdate.toDateString()} ${lastUpdate.toLocaleTimeString()}`)
 
-    var width = 600,
-        height = 600,
-        centered;
+    let width = d3.select(`#${SVG}`).node().parentNode.getBoundingClientRect().width * 0.9
+    let height = width
 
-    // Define color scale
-    var color = d3.scaleLinear()
-        .domain([1, 20])
-        .clamp(true)
-        .range(['#fff', '#409A99']);
+    await createMap(width, height)    
+}
 
-    var projection = d3.geoMercator()
+const createMap = async (width, height) => {
+    let projection = d3.geoMercator()
         .scale(1600)
-        // Center the Map in Colombia
         .center([-74, 4.5])
-        .translate([width / 2, height / 2]);
+        .translate([width / 2, height / 2])
 
-    var path = d3.geoPath()
-        .projection(projection);
+    let path = d3.geoPath()
+        .projection(projection)
 
-    // Set svg width & height
-    var svg = d3.select('#map')
+    let svg = d3.select(`#${SVG}`)
         .attr('width', width)
-        .attr('height', height);
+        .attr('height', height)
 
-    var mapLayer = svg.append('g');
+    let mapLayer = svg.append('g')
 
-    // Load map data
     let mapData = await d3.json(colombiaGeoJson)
-    var features = mapData.features;
-    console.log(features)
+    let features = mapData.features
 
-    // Update color scale domain based on data
-    // color.domain([0, d3.max(features, 10)]);
-
-    // Draw each province as a path
-    let paths = mapLayer.selectAll('path')
+    mapLayer.selectAll('path')
         .data(features)
         .enter().append('path')
         .attr('d', path)
@@ -46,39 +33,65 @@ const initialize = async _ => {
         .style('fill', 'white')
         .style('stroke', 'gray')
 
-    let data = await d3.csv(testData)
-
-    var waypointMap = new Waypoint({
-        element: document.getElementById('map'),
-        handler: function (direction) {
+    // Fix/unfix map
+    new Waypoint({
+        element: document.getElementById('chart'),
+        handler: direction => {
             if (direction === DOWN)
-                d3.select('#map').style('position', 'fixed').style('top', '10%')
+                d3.select('#chart').style('position', 'fixed').style('top', '10%')
             else if (direction === UP)
-                d3.select('#map').style('position', '').style('top', '')
+                d3.select('#chart').style('position', '').style('top', '')
         },
         offset: '10%'
     })
 
-    var waypointText1 = new Waypoint({
+    // Add/remove dots
+    new Waypoint({
         element: document.getElementById('text_1'),
-        handler: function (direction) {
-            if (direction === DOWN) {
+        handler: direction => {
+            if (direction === DOWN) 
                 mapLayer.selectAll('circle')
                     .data(features)
                     .enter().append('circle')
                     .attr('class', 'text_1')
                     .attr('r', 5)
-                    .attr("transform", d =>
-                        "translate(" + path.centroid(d) + ")"
+                    .attr('transform', d =>
+                        'translate(' + path.centroid(d) + ')'
                     )
                     .style('fill', '#047ab3')
-            }
-            else if (direction === UP) 
+            else if (direction === UP)
                 mapLayer.selectAll('.text_1')
                     .remove()
         },
-        offset: '10%'
-    })
+        offset: '40%'
+    })    
+    
+    // Add/remove color
+    new Waypoint({
+        element: document.getElementById('text_2'),
+        handler: direction => {
+            if (direction === DOWN) {
+                mapLayer.selectAll('path')
+                    .style('fill', d => palette[Math.round(Math.random()*19)])
+
+                mapLayer.selectAll('.text_1')
+                    .remove()
+            }
+            else if (direction === UP) {
+                mapLayer.selectAll('path')
+                    .style('fill', 'white')
+
+                mapLayer.selectAll('circle')
+                    .data(features)
+                    .enter().append('circle')
+                    .attr('class', 'text_1')
+                    .attr('r', 5)
+                    .attr('transform', d => `translate(${path.centroid(d)})`)
+                    .style('fill', '#047ab3')
+            }
+        },
+        offset: '40%'
+    })    
 }
 
 initialize()
