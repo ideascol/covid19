d2a2cadfaf10e0f0426557cb91da63f6ddaa94fb
mvanegas10 <m.vanegas10@uniandes.edu.co>
2020-05-05 13:05:08 +0200

    Add politico charts for departments

--- js/index.js
+++ js/index.js
@@ -1069,14 +1069,14 @@ const createMap = async (width, height) => {
             features.push({ ...feature, properties: { values: row, ...feature.properties } })
     })
 
-    let y = d3.scaleLinear()
-        .range([0, 3])
-        .domain([0, d3.max(data, d => d[COLS_DMNTOS['tests']])])
-
     let y = d3.scaleLog()
         .range([0, 10])
         .domain([1, d3.max(data, d => d[COLS_DMNTOS['tests']])])
-        
+
+    let svg = d3.select('#map').select('svg')
+        .attr('width', width)
+        .attr('height', height)
+
     let mapLayer = svg.append('g')
 
     mapLayer.selectAll('path')
